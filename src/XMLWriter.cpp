b9ffc051d4649b14791ed0a9e6e49430fde0e7df
prithika5 <prithika.thilak@gmail.com>
2026-01-31 20:33:35 +0000

    Add XMLWriter extra functions

--- src/XMLWriter.cpp
+++ src/XMLWriter.cpp
@@ -4,15 +4,57 @@
 #include <string>
 
 struct CXMLWriter::SImplementation {
-    
-    std::shared_ptr<CDataSink> DSink;
+
+    std::shared_ptr<CDataSink> Sink;
     std::vector<std::string> Open;
 
     SImplementation(std::shared_ptr<CDataSink> sink) {
-        DSink = sink;
+        Sink = sink;
     }
 
     ~SImplementation() {
+    }
+
+    bool PutStr(const std::string &s) {
+        std::vector<char> buf(s.begin(), s.end());
+        return Sink->Write(buf);
+    }
+
+    std::string EscText(const std::string &s) {
+        std::string out;
+        for (char c : s) {
+            if (c == '&') out += "&amp;";
+            else if (c == '<') out += "&lt;";
+            else if (c == '>') out += "&gt;";
+            else out += c;
+        }
+        return out;
+    }
+
+    std::string EscAttr(const std::string &s) {
+        std::string out;
+        for (char c : s) {
+            if (c == '&') out += "&amp;";
+            else if (c == '<') out += "&lt;";
+            else if (c == '>') out += "&gt;";
+            else if (c == '"') out += "&quot;";
+            else out += c;
+        }
+        return out;
+    }
+
+    bool PutAttr(const TAttributes &attrs) {
+        for (TAttributes::size_type i = 0; i < attrs.size(); i++) {
+            const std::string &k = attrs[i].first;
+            const std::string &v = attrs[i].second;
+
+            PutStr(" ");
+            PutStr(k);
+            PutStr("=\"");
+            PutStr(EscAttr(v));
+            PutStr("\"");
+        }
+        return true;
     }
 
     bool WriteEntity(const SXMLEntity &) {
